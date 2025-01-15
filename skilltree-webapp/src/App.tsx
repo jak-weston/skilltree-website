130f3bc4031c314f5215a6e2ecb56b594a5dd6a4
jak-weston <jak.w.weston@gmail.com>
2025-01-14 16:01:51 -0800

    Started work on website. QR code and phone mockups added.

--- skilltree-webapp/src/App.tsx
+++ skilltree-webapp/src/App.tsx
@@ -1,28 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FitBee from "./components/FitBee.tsx"; // Adjust the path based on your file structure
+import QR1 from "./images/QR1.png"; // Path to QR image
+import QR2 from "./images/QR2.png"; // Path to QR image
+import purpleHome from "./images/purple-home-screen.png";
+import redHome from "./images/red-home-screen.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FitBee qrImage={QR1} mockupImage={purpleHome} link={"https://docs.google.com/forms/d/e/1FAIpQLSerCnugvyEHpVKfFFnJYkFJ3XfDyJ4lEaUZC-j-AcxNtQ6pZA/viewform"} /> {/* Pass images as props */}
+      <FitBee qrImage={QR2} mockupImage={redHome} link={"https://docs.google.com/forms/d/e/1FAIpQLScl-8DE9oXmuOWI05_h7zFCP1INsQtsxDi6b6PtQu1NB1MQRg/viewform"} /> {/* Pass images as props */}
     </div>
   );
 }
-
-
 
 export default App;
