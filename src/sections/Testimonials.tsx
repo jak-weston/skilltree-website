a9d16d42d02f6d14cc1178e6f3eaa304106eef12
Isaac Morrow <isaacmorrowofficial@gmail.com>
2025-07-07 13:58:32 -0700

    feat: Added stars to testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,17 +1,47 @@
 import { HomeCopy as copy } from "@/content";
+import { cn } from "@/lib/utils";
+import { FaStar, FaQuoteRight } from "react-icons/fa6";
 
 export default function Testimonials() {
   return (
     <section
       id="testimonials"
-      className="w-full flex flex-col items-center justify-center"
+      className="w-full flex flex-col items-center justify-center spacing-section"
     >
-      <h2>{copy.testimonials.header}</h2>
-      <div className="flex flex-col gap-4 w-full max-w-2xl mt-4">
+      <div className="w-full flex flex-col items-center justify-center text-center">
+        <span className="chip">Testimonials</span>
+        <h2>{copy.testimonials.header}</h2>
+        <p>{copy.testimonials.subheader}</p>
+      </div>
+
+      <div className="flex flex-col gap-4 w-full md:container mt-4 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-6 ">
         {copy.testimonials.quotes.map((quote, index) => (
-          <div key={index} className="border p-4 rounded">
-            <p className="italic">{quote.text}</p>
-            <p className="mt-2 text-right">– {quote.author}</p>
+          <div
+            key={index}
+            className={cn(
+              "border p-4 md:p-6 relative rounded-lg",
+              index >= 4 ? "hidden md:block" : ""
+            )}
+          >
+            <FaQuoteRight
+              className="absolute top-2 right-2 text-secondary"
+              size={60}
+            />
+
+            <h5 className="-mb-1">{quote.author}</h5>
+
+            {Array.from({ length: 5 }, (_, i) => (
+              <FaStar
+                key={i}
+                className={
+                  i < quote.rating
+                    ? "text-yellow-500 inline-block mr-1"
+                    : "text-primary/50 inline-block mr-1"
+                }
+              />
+            ))}
+
+            <p className="mt-2 text-primary/90">{quote.text}</p>
           </div>
         ))}
       </div>
