50b8f91c38d32cbbdd74fa20286214ea53a09195
jak-weston <jak.w.weston@gmail.com>
2025-07-20 12:10:55 -0700

    forgot to add import

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import BetaTestSection from "@/components/BetaTestSignUp";
 import { FaRegClock, FaRegEnvelope, FaRegBell } from "react-icons/fa6";
 import { FaRegKeyboard } from "react-icons/fa";
+import { span } from "motion/react-client";
 
 export default function ComingSoon() {
   return (
